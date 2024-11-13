0ec9ddb2fc6fa15cabd6054e48a61c67a40fe598
styven <styvenn16@gmail.com>
2024-11-13 09:51:55 -0300

    Rotas da API

--- main.go
+++ main.go
@@ -15,14 +15,41 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	// l ista de Rotas
+	// lista de Rotas
 	e.GET("/students", getStudents)
+	e.POST("/students", createStudents)
+
+	e.GET("/students:id", getStudent)
+	e.PUT("/students:id", updateStudent)
+	e.DELETE("/students:id", deleteStudent)
 
 	//Inicio do servidor
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
+
+
+
 // Gerenciar  a rota do servidor
 func getStudents(c echo.Context) error {
 	return c.String(http.StatusOK, "List of all students")
+}
+
+func createStudents(c echo.Context) error {
+	return c.String(http.StatusOK, "Create students")
+}
+
+func getStudent(c echo.Context) error {
+	id:= c.Param("id")
+	return c.String(http.StatusOK, "Get student by id: "+id)
+}
+
+func updateStudent(c echo.Context) error {
+	id:= c.Param("id")
+	return c.String(http.StatusOK, "Update student by id: "+id)
+}
+
+func deleteStudent(c echo.Context) error {
+	id:= c.Param("id")
+	return c.String(http.StatusOK, "Deletar student by id: "+id)
 }
